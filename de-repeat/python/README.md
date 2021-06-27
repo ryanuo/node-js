e9b8bd5042e82379bf2b12cc7fe53b001b0f4f6a
Rr210 <1577791638@qq.com>
2021-06-27 13:40:45 +0800

    doc:update totdolist content

---
+++ de-repeat/python/README.md
@@ -0,0 +1,24 @@
+## 更新须知
+
+>新版本 1.3 [exe下载地址](https://gitee.com/rbozo/de-repeat/releases/)
+
+## 历史版本
+
+- 版本1.0   2020-06-24
+
+1. tkinter版本发布
+2. 使用方式:python运行执行
+
+- 版本1.1  2020-06-25
+
+1. 自定义appid
+2. 加入三种模式，`简单，中等，高级`
+3. 加入web版本
+
+- 版本1.2-1.3 2020-06-26
+
+1. 打包tkinter版本
+2. tkinter[exe](https://gitee.com/rbozo/de-repeat/releases/)版本发布
+3. 加入清空和复制功能
+4. 修复转换后文本内容叠加问题
+5. 加入空值判断
--- de-repeat/python/de-repeat.py
+++ de-repeat/python/de-repeat.py
@@ -1,10 +1,7 @@
-import hashlib
+import hashlib, random, pyperclip, requests, webbrowser
 import tkinter.font as tf
 import tkinter.messagebox
-from random import random
 from tkinter import *  # 使用Tkinter前需要先导入
-import requests
-import webbrowser
 
 '''
 tkinter版本论文降重
@@ -29,6 +26,7 @@ class Repeat:
             ],
         }
 
+    # 执行函数
     def run(self):
         appid = t1.get()
         key = t2.get()
@@ -38,9 +36,6 @@ class Repeat:
             lan_s = self.transList['middle']
         else:
             lan_s = self.transList['high']
-        print(lan_s)
-        print(appid, key)
-        # q = '将小程序运营规范放在突出的位置以显著的方式使用户、消费者知晓了解；同时也有义务将相关的法律、法规、规章等以显著的方式或链接的形式使用户知晓。'
         q = la3.get("1.0", "end")
         if len(appid) == 0 or len(key) == 0 or len(q) <= 1:
             tkinter.messagebox.showinfo(title='提示', message='Appid或者key或者转换的值不能为空')
@@ -59,9 +54,9 @@ class Repeat:
             }
             r = requests.get(url, params=params)
             txt = r.json()
-            print(txt)
             print(txt['trans_result'][0]['dst'])
             q = txt['trans_result'][0]['dst']
+        la2.delete(1.0, 'end')
         la2.insert('insert', q)
 
     # 设置清空按钮
@@ -71,6 +66,14 @@ class Repeat:
 
     def open_url(self, event):
         webbrowser.open("https://u.mr90.top/posts/644213/", new=0)
+
+    def copy_s(self):
+        text = la2.get(1.0, "end")
+        if len(text) > 1:
+            pyperclip.copy(la2.get(1.0, "end"))
+            tkinter.messagebox.showinfo(title='提示', message='复制成功')
+        else:
+            tkinter.messagebox.showinfo(title='提示', message='文本框为空!!')
 
 
 if __name__ == "__main__":
@@ -82,18 +85,15 @@ if __name__ == "__main__":
     # 绘制窗口内容
     iv_default = IntVar()
     iv_default.set(0)
-    # Repeat()
     # appid+key
     rb_appid_Label = Label(window, text='appid:')
     rb_key_Label = Label(window, text='key:')
-    # b = Button(window, text='登录', font=('Arial', 12), width=5, height=1, command=Repeat)
     t1 = Entry(window, show=None, bg='#F0FFF0', width=20)
     t2 = Entry(window, show=None, bg='#F0FFF0', width=20)
     rb_appid_Label.grid(row=1, column=1, ipadx=1, ipady=1, sticky='E', pady=10)
     rb_key_Label.grid(row=1, column=3, ipadx=1, ipady=1, sticky='E', pady=10)
     t1.grid(row=1, column=2, ipadx=1, pady=10)
     t2.grid(row=1, column=4, ipadx=1, pady=10)
-    # b.grid(row=1, column=5, padx=10, pady=10, sticky='W')
     # 选择降重等级
     rb_default_Label = Label(window, text='选择降重等级：')
     rb_default1 = Radiobutton(window, text='简单', value=0, variable=iv_default)
@@ -115,14 +115,16 @@ if __name__ == "__main__":
     la2.grid(row=4, column=2, columnspan=5, ipadx=40, pady=10)
     # 执行
     starts = Button(window, text='立即执行', font=('Arial', 12), width=10, height=1, command=Repeat().run)
-    starts.grid(row=5, column=1, columnspan=4)
+    starts.grid(row=5, column=1, columnspan=3)
     # 清空
     clear = Button(window, text='清空', font=('Arial', 12), width=10, height=1, command=Repeat().delete_text)
-    clear.grid(row=5, column=3, columnspan=4)
+    clear.grid(row=5, column=3, columnspan=1)
+    # 复制
+    copys = Button(window, text='复制', font=('Arial', 12), width=10, height=1, command=Repeat().copy_s)
+    copys.grid(row=5, column=4, columnspan=2)
     ft = tf.Font(family='黑体', size=10, underline=1)
     rb_right_Label = Label(window, foreground='blue', text='Copyright © 2020-2021 Harry. All rights reserved.', font=ft)
     rb_right_Label.grid(row=7, column=2, columnspan=4, pady=20)
-    # 此处必须注意，绑定的事件函数中必须要包含event参数
     # 绑定label单击时间
     rb_right_Label.bind("<Button-1>", Repeat().open_url)
     window.mainloop()
