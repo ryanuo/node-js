bd112cadc9419fcbe29c1166aa1c7742551a781e
Rr210 <1577791638@qq.com>
2021-08-10 19:17:34 +0800

    doc:update totdolist content

---
+++ vue/README.md
@@ -0,0 +1,25 @@
+# first-project
+
+> 购物车案例
+
+## Project setup
+
+```shell
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```shell
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```shell
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue/src/components/Left.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h5>我是左组件</h5>
+    <my-count :init="9" @numchange="getNewCount"></my-count>
+    {{message}}
+    <button @click="send">发送</button>
+  </div>
+</template>
+
+<script>
+import bus from '@/eventBus.js'
+export default {
+  data(){
+    return{
+      message:'',
+      str:"黑云压城城欲摧,驻青沙柏菲会,借问酒家何处是,半江瑟瑟半江红"
+    }
+  },
+  methods: {
+    getNewCount(val) {
+      this.message = val;
+    },
+    send(){
+      bus.$emit('sendstr',this.str)
+    }
+  },
+};
+</script>
+
+<style lang='less'>
+</style>
