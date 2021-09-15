4f6b038b0a29016c742f0254b3e02cdbf29e530e
rr210 <1577791638@qq.com>
2021-09-15 15:18:14 +0800

    doc:push vue3 todolist

---
+++ vue3/todolist/src/components/todo-button/TodoButton.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Description: 
+ * @Author: Harry
+ * @Date: 2021-09-15 14:41:11
+ * @Url: https://u.mr90.top
+ * @github: https://github.com/rr210
+ * @LastEditTime: 2021-09-15 15:00:06
+ * @LastEditors: Harry
+-->
+<template>
+  <div class="button-container mt-3">
+    <div class="btn-group">
+      <button
+        type="button"
+        class="btn"
+        :class="active === 0 ? 'btn-primary' : 'btn-secondary'"
+        @click="onBtnIndex(0)"
+      >
+        全部
+      </button>
+      <button
+        type="button"
+        class="btn"
+        :class="active === 1 ? 'btn-primary' : 'btn-secondary'"
+        @click="onBtnIndex(1)"
+      >
+        已完成
+      </button>
+      <button
+        type="button"
+        class="btn"
+        :class="active === 2 ? 'btn-primary' : 'btn-secondary'"
+        @click="onBtnIndex(2)"
+      >
+        未完成
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoButton",
+  emits: ["update:active"],
+  props: {
+    active: {
+      type: Number,
+      required: true,
+      // 默认激活索引值为 0 的按钮（全部：0，已完成：1，未完成：2）
+      default: 0,
+    },
+  },
+  methods: {
+    onBtnIndex(index) {
+      if (this.active == index) return;
+      this.$emit("update:active", index);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.button-container {
+  width: 400px;
+  text-align: center;
+}
+</style>
---
+++ vue3/todolist/src/components/todo-input/TodoInput.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Description: 
+ * @Author: Harry
+ * @Date: 2021-09-15 14:10:26
+ * @Url: https://u.mr90.top
+ * @github: https://github.com/rr210
+ * @LastEditTime: 2021-09-15 14:49:01
+ * @LastEditors: Harry
+-->
+
+<template>
+  <div>
+    <!-- form 表单 -->
+    <form class="form-inline" @submit.prevent="onFormSubmit">
+      <div class="input-group mb-2 mr-sm-2">
+        <!-- 输入框的前缀 -->
+        <div class="input-group-prepend">
+          <div class="input-group-text">任务</div>
+        </div>
+        <!-- 文本输入框 -->
+        <input
+          type="text"
+          class="form-control"
+          placeholder="请填写任
+务信息"
+          style="width: 356px"
+          v-model.trim="taskname"
+        />
+      </div>
+
+      <!-- 添加按钮 -->
+      <button type="submit" class="btn btn-primary mb-2">添加新任务</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoInput",
+  // 声明自定义事件
+  emits: "add",
+  data() {
+    return {
+      taskname: "",
+    };
+  },
+  methods: {
+    onFormSubmit(e) {
+      if (!this.taskname) return alert("任务名称不能为空");
+      this.$emit("add", this.taskname);
+      this.taskname = "";
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
