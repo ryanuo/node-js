ff6714357b7ec4d887480311fb8e4c468c4f2e68
rr210 <1577791638@qq.com>
2021-12-04 22:08:42 +0800

    doc:update ts-vue3-element-plus content

--- ts/vue3-ts-elementplus/vue.config.js
+++ ts/vue3-ts-elementplus/vue.config.js
@@ -4,7 +4,7 @@
  * @Date: 2021-11-28 16:20:16
  * @Url: https://u.mr90.top
  * @github: https://github.com/rr210
- * @LastEditTime: 2021-12-02 21:26:38
+ * @LastEditTime: 2021-12-04 17:20:31
  * @LastEditors: Harry
  */
 module.exports = {
@@ -19,10 +19,13 @@ module.exports = {
           // 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/user/userInfo 时
           // 实际上访问的地址是：http://192.168.1.249:9527/user/userInfo,因为重写了 /api
           '^/proxy': ''
-        },
-        onProxyReq(proxyReq, req, res) {
-          // 设置cookie
-          proxyReq.setHeader('cookie', "SESSION=ZT-f4453be3-f1fd-4997-a4fb-98dd6fc8839e")
+        }
+      },
+      "/uploads": {
+        target: 'https://kc.mr90.top/predict_image', //对应自己的接口，代理地址修改后必须重启项目
+        changeOrigin: true, //是否允许跨域
+        pathRewrite: {
+          '^/uploads': ''
         }
       }
     }
