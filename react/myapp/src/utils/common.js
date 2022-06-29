b037a54bbdf0f3be3146a097c692bca615f06dea
rr210 <1577791638@qq.com>
2022-06-29 19:54:03 +0800

    fix:修改图片路径

--- react/myapp/src/utils/common.js
+++ react/myapp/src/utils/common.js
@@ -3,8 +3,8 @@
  * @Date: 2022-06-29 18:16:47
  * @LastEditors: harry
  * @Github: https://github.com/rr210
- * @LastEditTime: 2022-06-29 18:22:03
- * @FilePath: \myapp\src\utils\common.js
+ * @LastEditTime: 2022-06-29 19:48:35
+ * @FilePath: \node-js\react\myapp\src\utils\common.js
  */
 export const mealDatas = [
     {
@@ -12,45 +12,45 @@ export const mealDatas = [
         title: '汉堡包',
         desc: '百分百纯牛肉配搭爽脆酸瓜洋葱粒与美味番茄酱经典滋味让你无法抵挡！',
         price: 12,
-        img: '/img/meals/1.png'
+        img: './img/meals/1.png'
     },
     {
         id: '2',
         title: '双层吉士汉堡',
         desc: '百分百纯牛肉与双层香软芝，加上松软面包及美味酱料，诱惑无人能挡！',
         price: 20,
-        img: '/img/meals/2.png'
+        img: './img/meals/2.png'
     },
     {
         id: '3',
         title: '巨无霸',
         desc: '两块百分百纯牛肉，搭配生菜、洋葱等新鲜食材，口感丰富，极致美味！',
         price: 24,
-        img: '/img/meals/3.png'
+        img: './img/meals/3.png'
     }, {
         id: '4',
         title: '麦辣鸡腿汉堡',
         desc: '金黄脆辣的外皮，鲜嫩幼滑的鸡腿肉，多重滋味，一次打动您挑剔的味蕾！',
         price: 21,
-        img: '/img/meals/4.png'
+        img: './img/meals/4.png'
     }, {
         id: '5',
         title: '板烧鸡腿堡',
         desc: '原块去骨鸡排嫩滑多汁，与翠绿新鲜的生菜和香浓烧鸡酱搭配，口感丰富！',
         price: 22,
-        img: '/img/meals/5.png'
+        img: './img/meals/5.png'
     }, {
         id: '6',
         title: '麦香鸡',
         desc: '清脆爽口的生菜，金黄酥脆的鸡肉。营养配搭，好滋味的健康选择！',
         price: 14,
-        img: '/img/meals/6.png'
+        img: './img/meals/6.png'
     }, {
         id: '7',
         title: '吉士汉堡包',
         desc: '百分百纯牛肉与香软芝士融为一体配合美味番茄醬丰富口感一咬即刻涌现！',
         price: 12,
-        img: '/img/meals/7.png'
+        img: './img/meals/7.png'
     }
 ];
 
